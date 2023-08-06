1a8363463b809f0b327c695e9aa4005938eddc6e
Ling-yunchi <wr020210@gmail.com>
2023-08-06 18:42:51 +0800

    feat: Lesson 3: Hidden faces removal (z buffer)

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,4 @@
 mod draw;
 mod vec;
 
-use crate::draw::{triangle};
-
-use tgaimage::{TGAColor, TGAImage};
-use crate::vec::Vec2i;
-
-fn main() {
-    let width = 200;
-    let height = 200;
-    let mut image = TGAImage::new(width, height, 3);
-    triangle(
-        Vec2i::new(10, 10),
-        Vec2i::new(100, 30),
-        Vec2i::new(190, 160),
-        &mut image,
-        &TGAColor::rgb(255, 0, 0),
-    );
-    image.flip_vertically();
-    image.write_tga_file("output.tga", true);
-}
+fn main() {}
--- src/vec.rs
+++ src/vec.rs
@@ -1,6 +1,78 @@
 use std::ops::{Add, Mul, Sub};
 
-pub trait Vec {
+pub struct Vec<const SIZE: usize, T: Default + Copy> {
+    data: [T; SIZE],
+}
+
+impl<const SIZE: usize, T: Default + Copy> Vec<SIZE, T> {
+    pub fn new() -> Self {
+        Self {
+            data: [T::default(); SIZE],
+        }
+    }
+}
+
+impl<const SIZE: usize, T: Default + Copy> Add for Vec<SIZE, T>
+where
+    T: Add<Output = T>,
+{
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let mut result = Self::new();
+        for i in 0..SIZE {
+            result.data[i] = self.data[i] + rhs.data[i];
+        }
+        result
+    }
+}
+
+impl<const SIZE: usize, T: Default + Copy> Sub for Vec<SIZE, T>
+where
+    T: Sub<Output = T>,
+{
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let mut result = Self::new();
+        for i in 0..SIZE {
+            result.data[i] = self.data[i] - rhs.data[i];
+        }
+        result
+    }
+}
+
+impl<const SIZE: usize, T: Default + Copy> Mul<f32> for Vec<SIZE, T>
+where
+    T: Mul<f32, Output = T>,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        let mut result = Self::new();
+        for i in 0..SIZE {
+            result.data[i] = self.data[i] * rhs;
+        }
+        result
+    }
+}
+
+impl<const SIZE: usize, T: Default + Copy> Mul for Vec<SIZE, T>
+where
+    T: Mul<Output = T>,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        let mut result = Self::new();
+        for i in 0..SIZE {
+            result.data[i] = self.data[i] * rhs.data[i];
+        }
+        result
+    }
+}
+
+pub trait Vector {
     fn cross(self, rhs: Self) -> Self;
     fn normalize(self) -> Self;
 }
@@ -17,9 +89,12 @@ impl Vec2i {
     }
 }
 
-impl Vec for Vec2i {
+impl Vector for Vec2i {
     fn cross(self, rhs: Self) -> Self {
-        Self::new(self.x * rhs.y - self.y * rhs.x, self.y * rhs.x - self.x * rhs.y)
+        Self::new(
+            self.x * rhs.y - self.y * rhs.x,
+            self.y * rhs.x - self.x * rhs.y,
+        )
     }
 
     fn normalize(self) -> Self {
@@ -53,6 +128,78 @@ impl Mul<f32> for Vec2i {
     }
 }
 
+impl Mul for Vec2i {
+    type Output = i32;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        self.x * rhs.x + self.y * rhs.y
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Vec2f {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl Vec2f {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self { x, y }
+    }
+}
+
+impl Vector for Vec2f {
+    fn cross(self, rhs: Self) -> Self {
+        Self::new(
+            self.x * rhs.y - self.y * rhs.x,
+            self.y * rhs.x - self.x * rhs.y,
+        )
+    }
+
+    fn normalize(self) -> Self {
+        let len = (self.x * self.x + self.y * self.y).sqrt();
+        Self::new(self.x / len, self.y / len)
+    }
+}
+
+impl Add for Vec2f {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(self.x + rhs.x, self.y + rhs.y)
+    }
+}
+
+impl Sub for Vec2f {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::new(self.x - rhs.x, self.y - rhs.y)
+    }
+}
+
+impl Mul<f32> for Vec2f {
+    type Output = Self;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self::new(self.x * rhs, self.y * rhs)
+    }
+}
+
+impl Mul for Vec2f {
+    type Output = f32;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        self.x * rhs.x + self.y * rhs.y
+    }
+}
+
+impl From<[f32; 2]> for Vec2f {
+    fn from(data: [f32; 2]) -> Self {
+        Self::new(data[0], data[1])
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 pub struct Vec3f {
     pub x: f32,
@@ -66,7 +213,7 @@ impl Vec3f {
     }
 }
 
-impl Vec for Vec3f {
+impl Vector for Vec3f {
     fn cross(self, rhs: Self) -> Self {
         Self::new(
             self.y * rhs.z - self.z * rhs.y,
